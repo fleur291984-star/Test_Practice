4fcaed611eeabbf7daacadb02a1c1b3b7874e7f2
Marina_Parfenova <fleur291984@gmail.com>
2026-04-30 19:06:43 +0300

    .gitignore is changed

--- src/test/java/user/UserFactory.java
+++ src/test/java/user/UserFactory.java
@@ -1,42 +1,35 @@
 package user;
 
-import io.qameta.allure.Step;
 import utils.PropertyReader;
 
 public class UserFactory {
 
-    @Step("Подготовить данные администратора из конфига")
     public static User withAdminPermission() {
         return new User(
                 PropertyReader.getProperty("saucedemoo.user"),
                 PropertyReader.getProperty("saucedemoo.password"));
     }
 
-    @Step("Подготовить данные заблокированного пользователя")
     public static User withLockedPermission() {
         return new User(
                 PropertyReader.getProperty("saucedemoo.locked.user"),
                 PropertyReader.getProperty("saucedemoo.password"));
     }
 
-    @Step("Подготовить данные несуществующего пользователя")
     public static User withIncorrectPermission() {
         return new User(
                 PropertyReader.getProperty("saucedemoo.incorrect.user"),
                 PropertyReader.getProperty("saucedemoo.password"));
     }
 
-    @Step("Подготовить данные пользователя с пустым полем логин")
     public static User withEmptyLogin() {
         return new User("", PropertyReader.getProperty("saucedemoo.password"));
     }
 
-    @Step("Подготовить данные пользователя с пустым полем пароль")
     public static User withEmptyPassword() {
         return new User(PropertyReader.getProperty("saucedemoo.user"), "");
     }
 
-    @Step("Подготовить персональные данные для оформления заказа")
     public static User withCheckoutInfo() {
         return new User(
                 PropertyReader.getProperty("saucedemoo.first.name"),
@@ -44,19 +37,16 @@ public class UserFactory {
                 PropertyReader.getProperty("saucedemoo.postal.code"));
     }
 
-    @Step("Подготовить данные пользователя с пустым полем Имя")
     public static User withEmptyFirstName() {
         return new User("", PropertyReader.getProperty("saucedemoo.last.name"),
                 PropertyReader.getProperty("saucedemoo.postal.code"));
     }
 
-    @Step("Подготовить данные пользователя с пустым полем Фамилия")
     public static User withEmptyLastName() {
         return new User(PropertyReader.getProperty("saucedemoo.first.name"),
                 "", PropertyReader.getProperty("saucedemoo.postal.code"));
     }
 
-    @Step("Подготовить данные пользователя с пустым полем Индекс")
     public static User withEmptyPostalCode() {
         return new User(PropertyReader.getProperty("saucedemoo.first.name"),
                 PropertyReader.getProperty("saucedemoo.last.name"), "");
